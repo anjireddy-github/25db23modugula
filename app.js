63e2a0d8eb0ac69ead89ce919f7332a0c7625f3b
anjireddy-github <modugulaanjireddy18@gmail.com>
2025-11-21 09:11:43 -0600

    Implement user authentication with Passport.js and add Account model

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -24,8 +26,28 @@ var pickRouter = require('./routes/pick');
 var resourceRouter = require('./routes/resource');
 
 var Concert = require("./models/concert");
+var Account = require('./models/account');
 
 var app = express();
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user){
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err){
+        return done(err)
+      })
+  }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,6 +57,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -43,6 +74,13 @@ app.use('/concerts', concertsRouter);
 app.use('/grid', gridRouter);
 app.use('/selector', pickRouter);
 app.use('/resource', resourceRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
