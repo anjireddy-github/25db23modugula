69c25cf97d9d61787387a786c964b7a5853fccb8
anjireddy-github <modugulaanjireddy18@gmail.com>
2025-12-03 18:46:10 -0600

    Add authentication protection to update endpoint

--- routes/concerts.js
+++ routes/concerts.js
@@ -2,6 +2,15 @@ var express = require('express');
 const concert_controlers= require('../controllers/concert');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+// or redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+    return next();
+  }
+  res.redirect("/login");
+}
+
 /* GET concerts home page - list all concerts */
 router.get('/', concert_controlers.concert_view_all_Page);
 
@@ -12,7 +21,7 @@ router.get('/create', concert_controlers.concert_create_Page);
 router.get('/detail', concert_controlers.concert_view_one_Page);
 
 /* GET update page for one concert */
-router.get('/update', concert_controlers.concert_update_Page);
+router.get('/update', secured, concert_controlers.concert_update_Page);
 
 /* GET delete confirmation page for one concert */
 router.get('/delete', concert_controlers.concert_delete_Page);
